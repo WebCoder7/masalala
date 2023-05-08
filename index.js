c6123926c6a5f528b92ed7f932f73cc987db5776
WebCoder7 <gulomovgulomjon296@gmail.com>
2023-05-08 20:24:58 +0500

    jj

---
+++ index.js
@@ -0,0 +1,30 @@
+ let ar = arr[a, b, c, d, e]
+    if(arr.ar > arr.b && arr.ar > arr.c && arr.ar > arr.d){
+        console.log(arr.ar);
+    }else if(arr.b > arr.ar && arr.b > arr.c && arr.b > arr.d){
+        console.log(arr.b);
+    }else if(arr.c > arr.ar && arr.c > arr.b && arr.c > arr.d){
+        console.log(arr.c);
+    }else{
+        console.log(arr.d);
+    }
+ar
+console.log(arr[1,5,9,10]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
